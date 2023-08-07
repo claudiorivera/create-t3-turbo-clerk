0d4fa2b045fe9fca1572368b595d1f202cafd7c0
Claudio Rivera <me@claudiorivera.com>
2023-08-06 21:31:31 -0500

    🚧 Add basic expo auth example

--- apps/expo/src/app/index.tsx
+++ apps/expo/src/app/index.tsx
@@ -1,79 +1,45 @@
 import React from "react";
-import { Button, Text, TouchableOpacity, View } from "react-native";
+import { Button, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Stack, useRouter } from "expo-router";
-import { FlashList } from "@shopify/flash-list";
+import { Stack } from "expo-router";
+import { SignedIn, SignedOut, useAuth } from "@clerk/clerk-expo";
 
-import type { RouterOutputs } from "~/utils/api";
-import { api } from "~/utils/api";
+import AuthExample from "~/components/AuthExample";
+import SignIn from "~/components/SignIn";
 
-function PostCard(props: {
-	post: RouterOutputs["post"]["all"][number];
-	onDelete: () => void;
-}) {
-	const router = useRouter();
-
-	return (
-		<View className="flex flex-row rounded-lg bg-white/10 p-4">
-			<View className="flex-grow">
-				<TouchableOpacity onPress={() => router.push(`/post/${props.post.id}`)}>
-					<Text className="text-xl font-semibold text-pink-400">
-						{props.post.title}
-					</Text>
-					<Text className="mt-2 text-white">{props.post.content}</Text>
-				</TouchableOpacity>
-			</View>
-			<TouchableOpacity onPress={props.onDelete}>
-				<Text className="font-bold uppercase text-pink-400">Delete</Text>
-			</TouchableOpacity>
-		</View>
-	);
-}
+const SignOut = () => {
+  const { isLoaded, signOut } = useAuth();
+  if (!isLoaded) {
+    return null;
+  }
+  return (
+    <View>
+      <Button
+        title="Sign Out"
+        onPress={() => {
+          signOut();
+        }}
+      />
+    </View>
+  );
+};
 
 const Index = () => {
-	const utils = api.useContext();
-
-	const postQuery = api.post.all.useQuery();
-
-	const deletePostMutation = api.post.delete.useMutation({
-		onSettled: () => utils.post.all.invalidate(),
-	});
-
-	return (
-		<SafeAreaView className="bg-[#1F104A]">
-			{/* Changes page title visible on the header */}
-			<Stack.Screen options={{ title: "Home Page" }} />
-			<View className="h-full w-full p-4">
-				<Text className="mx-auto pb-2 text-5xl font-bold text-white">
-					Create <Text className="text-pink-400">T3</Text> Turbo
-				</Text>
-
-				<Button
-					onPress={() => void utils.post.all.invalidate()}
-					title="Refresh posts"
-					color={"#f472b6"}
-				/>
-
-				<View className="py-2">
-					<Text className="font-semibold italic text-white">
-						Press on a post
-					</Text>
-				</View>
-
-				<FlashList
-					data={postQuery.data}
-					estimatedItemSize={20}
-					ItemSeparatorComponent={() => <View className="h-2" />}
-					renderItem={(p) => (
-						<PostCard
-							post={p.item}
-							onDelete={() => deletePostMutation.mutate(p.item.id)}
-						/>
-					)}
-				/>
-			</View>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView className="bg-[#1F104A]">
+      {/* Changes page title visible on the header */}
+      <Stack.Screen options={{ title: "Home Page" }} />
+      <View className="h-full w-full p-4">
+        <SignedIn>
+          <AuthExample />
+          <SignOut />
+        </SignedIn>
+        <SignedOut>
+          <SignIn />
+        </SignedOut>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default Index;
