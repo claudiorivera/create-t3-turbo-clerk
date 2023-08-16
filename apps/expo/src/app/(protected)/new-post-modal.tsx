28bb25582a1e6fd09be89d3ddfc551aa892442fb
Claudio Rivera <claudio@foundrymakes.com>
2023-08-16 15:00:12 -0500

    ✨ Add back button to modal

--- apps/expo/src/app/(protected)/new-post-modal.tsx
+++ apps/expo/src/app/(protected)/new-post-modal.tsx
@@ -1,9 +1,14 @@
-import { SafeAreaView, Text } from "react-native";
+import { Pressable, SafeAreaView, Text } from "react-native";
+import { Link } from "expo-router";
 
 export default function NewPostModal() {
 	return (
-		<SafeAreaView>
-			<Text>New Post</Text>
+		<SafeAreaView className="flex-1 justify-center">
+			<Link asChild href="..">
+				<Pressable>
+					<Text>Back</Text>
+				</Pressable>
+			</Link>
 		</SafeAreaView>
 	);
 }
