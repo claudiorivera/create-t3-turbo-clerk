509080a8b3e65e4b8f2672d4482bf0b7ff90ff76
Claudio Rivera <claudio@foundrymakes.com>
2023-08-18 14:58:55 -0500

    🚸 Make button use a pointer cursor

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
 	return (
 		<div className="flex h-screen flex-col gap-4 p-4">
 			<SignOutButton>
-				<div className="w-full rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
+				<div className="w-full cursor-pointer rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700">
 					Sign Out
 				</div>
 			</SignOutButton>
