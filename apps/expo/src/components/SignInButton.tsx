0fb8ef80658b3f20724bae1c6aa7bcae7ae2e4f4
Claudio Rivera <claudio@foundrymakes.com>
2023-08-28 10:58:48 -0500

    🚨 Run lint fix

--- apps/expo/src/components/SignInButton.tsx
+++ apps/expo/src/components/SignInButton.tsx
@@ -1,4 +1,5 @@
-import React, { ReactNode } from "react";
+import type { ReactNode } from "react";
+import React from "react";
 import * as WebBrowser from "expo-web-browser";
 import { useOAuth } from "@clerk/clerk-expo";
 
